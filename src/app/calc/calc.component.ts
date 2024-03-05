ec385570d4e07c54cab4eb987b5ea6f365832a2e
Felipe Garcia <felipe.garcia45@fatec.sp.gov.br>
2024-03-05 17:18:07 -0300

    Adicionei a calc

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrl: './calc.component.css'
+})
+export class CalcComponent {
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
+  calc (operador: string) {
+    switch(operador){
+      case'+':
+      this.resultado = Number(this.num1) + Number(this.num2);
+      break;
+      case'-':
+      this.resultado = Number(this.num1) - Number(this.num2);
+      break;
+      case'x':
+      this.resultado = Number(this.num1) * Number(this.num2);
+      break;
+      case'÷':
+      this.resultado = Number(this.num1) / Number(this.num2);
+      break;
+
+  }
+}
+}
